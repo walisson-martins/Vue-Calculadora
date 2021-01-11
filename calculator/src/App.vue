e18022ad368c89708cbf4de47365c07cf15c6bb4
walisson-martins <59905184+walisson-martins@users.noreply.github.com>
2021-01-11 17:35:41 -0300

    Projeto calculadora com VueJs.

---
+++ calculator/src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <div id="app">
+    <h1>Calculadora!</h1>
+    <Calculator />
+  </div>
+</template>
+
+<script>
+import Calculator from "./main/Calculator";
+export default {
+  components: { Calculator },
+};
+</script>
+
+<style>
+@font-face {
+  font-family: "roboto";
+  src: url("./fonts/roboto.ttf");
+}
+
+* {
+  font-family: "roboto";
+}
+
+body {
+  margin: 0;
+}
+
+#app {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+
+  color: #ffffff;
+  background: linear-gradient(to right, rgb(83, 105, 118), rgb(41, 46, 73));
+}
+</style>
